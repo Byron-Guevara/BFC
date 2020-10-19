5896cd652234e9ed22c55e915d6c4be17d5a0827
JorgeQZ <jorge_qzg@hotmail.com>
2020-10-19 12:33:37 -0500

    menu js functions

--- js/generals.js
+++ js/generals.js
@@ -1,24 +1,5 @@
 
 $(document).ready(function () {
-
-    $('.burguer').on('click', function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-
-        $(this).toggleClass('active');
-        $('.mobile-cont-menu').toggleClass('active');
-    });
-
-    let button = '<button class="submenu-button"><div>+</div></button>';
-    $('.menu-item-has-children').append(button);
-    $('.submenu-button').on('click', function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        $(this).toggleClass('active');
-        $(this).siblings('.sub-menu').slideToggle(500, "linear");
-    });
-
-
     $('.sliderServicios').owlCarousel({
         loop: false,
         margin: 10,
